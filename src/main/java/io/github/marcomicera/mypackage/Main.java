16bbad805b88b1f72a0c708d0ac89e77e2d0256c
Marco Micera <marco.micera@gmail.com>
2021-11-27 19:10:39 +0100

    Added missing JavaDoc comments

--- src/main/java/io/github/marcomicera/mypackage/Main.java
+++ src/main/java/io/github/marcomicera/mypackage/Main.java
@@ -2,9 +2,17 @@ package io.github.marcomicera.mypackage;
 
 public final class Main {
 
+    /**
+     * Restricting object creation.
+     */
     private Main() {
     }
 
+    /**
+     * Program entrypoint.
+     *
+     * @param args
+     */
     public static void main(final String[] args) {
         System.out.println("Hello, world!");
     }
--- src/main/java/io/github/marcomicera/mypackage/package-info.java
+++ src/main/java/io/github/marcomicera/mypackage/package-info.java
@@ -1,1 +1,4 @@
+/**
+ * Package info for the source code root.
+ */
 package io.github.marcomicera.mypackage;
